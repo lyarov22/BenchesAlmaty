fcd2f440788ac33e347f85cf45244b50551bb75c
Фёдоров Илья <96327924+lyarov22@users.noreply.github.com>
2023-12-12 13:46:08 +0600

    0.81 benches in map system v1

--- mySite/views.py
+++ mySite/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from benches.models import Bench
 
 from userSystem.models import Profile
 
 def index(request):
+    benches = Bench.objects.all()
+
     if request.user.is_authenticated:
         try:
             profile = Profile.objects.get(user=request.user)
@@ -11,9 +14,9 @@ def index(request):
             profile = Profile(user=request.user)
             profile.save()
         profile = Profile.objects.get(user=request.user)
-        return render(request, "mySite/index.html", {'profile': profile})
+        return render(request, "mySite/index.html", {'profile': profile, 'benches': benches})
     else:
-        return render(request, "mySite/index.html")
+        return render(request, "mySite/index.html", {'benches': benches})
 
 
 # ПОФИКСИТЬ КОСТЫЛЬ С ОТОБРАЖЕНИЕМ ФОТО, СДЕЛАТЬ ПРОВЕРКУ В ШАБЛОНЕ
