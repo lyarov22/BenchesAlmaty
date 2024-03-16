f1a9dd05be257d29971cf0bbb176e70006dcafb9
Фёдоров Илья <dyudyafed006@gmail.com>
2024-03-16 14:01:58 +0500

    Update wsgi.py

--- BenchesAlmaty/wsgi.py
+++ BenchesAlmaty/wsgi.py
@@ -7,8 +7,8 @@ from BenchesAlmaty import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BenchesAlmaty.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'BenchesAlmaty', 'staticfiles'))
-application.add_files(os.path.join(os.path.dirname(__file__), 'media'), prefix='media/')  # Если у вас есть медиа файлы
+application = WhiteNoise(application, root=os.path.join('staticfiles'))
+application.add_files(os.path.join('media'), prefix='media/')  # Если у вас есть медиа файлы
 
 # Enable insecure serving of static files when DEBUG is True
 if settings.DEBUG:
