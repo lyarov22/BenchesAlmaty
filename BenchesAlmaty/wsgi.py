fbe10da519141ecbaf54093c2269a963a7be16e6
Фёдоров Илья <dyudyafed006@gmail.com>
2024-03-16 13:37:47 +0500

    wsgi fix 1

--- BenchesAlmaty/wsgi.py
+++ BenchesAlmaty/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for BenchesAlmaty project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+from BenchesAlmaty import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BenchesAlmaty.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'BenchesAlmaty', 'staticfiles'))
+application.add_files(os.path.join(os.path.dirname(__file__), 'media'), prefix='media/')  # Если у вас есть медиа файлы
+
+# Enable insecure serving of static files when DEBUG is True
+if settings.DEBUG:
+    from django.contrib.staticfiles.handlers import StaticFilesHandler
+    application = StaticFilesHandler(application)
